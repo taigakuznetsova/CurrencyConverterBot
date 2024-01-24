0050c8f2420136f404bbf82726566497316e47eb
Анастасия Нагаева <taiga.kuznetsova@yandex.ru>
2024-01-24 11:14:11 +0300

    A new file is added

--- extensions.py
+++ extensions.py
@@ -29,6 +29,6 @@ class CryptoConverter:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
+        total_base = json.loads(r.content)[keys[base]] * amount
 
         return total_base
